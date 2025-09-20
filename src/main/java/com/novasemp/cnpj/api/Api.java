5a25473eda8db2a2eb067a463fcb6f4910070310
Vinicius Pereira de Jesus Junior <vinicius.282@hotmail.com>
2025-09-20 20:46:58 -0300

    Correção código API

--- src/main/java/com/novasemp/cnpj/api/Api.java
+++ src/main/java/com/novasemp/cnpj/api/Api.java
@@ -8,7 +8,6 @@ import spark.Spark;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
