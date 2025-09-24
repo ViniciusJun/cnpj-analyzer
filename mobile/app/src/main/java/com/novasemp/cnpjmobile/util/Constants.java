242a6ac73cbd5be940fa1a328365af5e0ef80da3
Vinicius Pereira de Jesus Junior <vinicius.282@hotmail.com>
2025-09-23 23:16:37 -0300

    Alteração porta API

--- mobile/app/src/main/java/com/novasemp/cnpjmobile/util/Constants.java
+++ mobile/app/src/main/java/com/novasemp/cnpjmobile/util/Constants.java
@@ -2,7 +2,7 @@ package com.novasemp.cnpjmobile.util;
 
 public class Constants {
     // Para emulador Android:
-    public static final String BASE_URL = "http://10.0.2.2:8080/";
+    public static final String BASE_URL = "http://10.0.2.2:8081/";
 
     // Para dispositivo físico (substitua pelo IP da sua máquina):
     // public static final String BASE_URL = "http://192.168.1.100:8080/";
